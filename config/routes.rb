6a92ae73e91d6040f3964d6e43bb34e2df4bb6c1
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-06-11 14:07:36 +0000

    kadaiupdate

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'top' => 'homes#top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
+  get 'homes' => 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
