e6682825ef92c8c350793ee2a2c593a5325ef08b
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-06-12 09:19:53 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,45 @@
 class BooksController < ApplicationController
   def index
-  end
-
-  def show
+    @book = Book.new
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
+    flash[:notice] = "successfully"
   end
 
   def create
-    @book = Book.new
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+    flash[:notice] = "successfully"
+  end
+
+  def show
+     @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+    flash[:notice] = "successfully"
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    flash[:notice] = "successfully"
+  end
+
+   private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
